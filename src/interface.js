e551dde4b8a80e1483ebcd362a1e92294e2780ae
tmampa <tmampa94@gmail.com>
2022-01-05 12:12:46 +0200

    Add functionality to the comment button

--- src/interface.js
+++ src/interface.js
@@ -1,21 +1,29 @@
-import { getShow } from './filmInfo.js';
+import { getShow } from "./filmInfo.js";
+import displayPopup from './engagement.js';
 
 export const loadInterface = () => {
-  const showContainer = document.querySelector('.container');
+  const showContainer = document.querySelector(".container");
   getShow().then((showArray) => {
     showArray.forEach((show) => {
-      const showCard = document.createElement('li');
-      showCard.className = 'film';
-      showCard.setAttribute('id', `${show.id}`);
+      const showCard = document.createElement("li");
+      showCard.className = "film";
+      showCard.setAttribute("id", `${show.id}`);
       showCard.innerHTML = `<img class="poster" src="${show.image.medium}">
       <div class="card-header">
       <p>${show.name}</p>
       <img class="like-icon">
       </div>
       <p>Likes</p>
-      <button type="button" class="comment-popup">Comments</button>
+      <button data-id="${shows[i].show.id}" class="btn-comments">Comments</button>
       <button type="button" class="rent-popup">Rent</button>`;
       showContainer.appendChild(showCard);
+      const button = document.querySelectorAll(
+        `[data-id="${show[i].show.id}"]`
+      )[0];
+      button.addEventListener("click", (e) => {
+        const showId = e.target.getAttribute("data-id");
+        displayPopup(showId);
+      });
     });
   });
 };
