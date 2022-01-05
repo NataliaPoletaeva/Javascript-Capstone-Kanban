611959b7fe8d25ebbb2cdf40faf9d5607857d72b
tmampa <tmampa94@gmail.com>
2022-01-05 14:20:59 +0200

    Remove base links

--- src/api.js
+++ src/api.js
@@ -1,7 +1,3 @@
-const genre = 'animation';
-const linkBase = 'https://api.tvmaze.com/shows/';
-const searchURL = `https://api.tvmaze.com/search/shows?q=${genre}`;
-
 const pullMovies = async () => {
   const response = await fetch(searchURL);
   const shows = await response.json();
